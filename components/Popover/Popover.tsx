83dc3039c7ffd55c53a7772858a9ea22e6a89556
Adem Enes Polat <ademenespolat@gmail.com>
2024-10-25 23:48:20 +0300

    popover position fixes..

--- components/Popover/Popover.tsx
+++ components/Popover/Popover.tsx
@@ -1,11 +1,44 @@
+import { useEffect, useRef } from 'react'
 import { PopoverProps } from '.'
 import styles from './Popover.module.css'
 
 const Popover = ({ children, label, placement = "down" }: PopoverProps) => {
+    const detail = useRef<HTMLDetailsElement>(null)
+    const popover = useRef<HTMLDivElement>(null)
+    const rect = detail.current?.getBoundingClientRect();
+    
+    const arrange = (rect: DOMRect | undefined) => {
+        if (!popover.current || !rect) return
+
+        if (placement === "down") {
+            popover.current.style.top = `${rect.y + rect.height + 5}px`;
+            popover.current.style.left = `${rect.left - popover.current.offsetWidth + rect.width}px`;
+        } else if (placement === "up") {
+            popover.current.style.top = `${rect.y - popover.current.offsetHeight - 5}px`;
+            popover.current.style.left = `${rect.left - popover.current.offsetWidth + rect.width}px`;
+        } else if (placement === "left") {
+            popover.current.style.top = `${rect.y}px`;
+            popover.current.style.left = `${rect.x - popover.current.offsetWidth - 5}px`;
+        } else if (placement === "right") {
+            popover.current.style.top = `${rect.y}px`;
+            popover.current.style.left = `${rect.x + rect.width + 5}px`;
+        }
+    }
+
+    useEffect(() => {
+        const handleScroll = () => {
+            arrange(detail.current?.getBoundingClientRect())
+        }
+        window.addEventListener("scroll", handleScroll)
+        return () => {
+            window.removeEventListener("scroll", handleScroll)
+        }
+    }, [rect]);
+
     return (
-        <details className={styles["popover-container"]} data-popover={placement}>
+        <details ref={detail} className={styles["popover-container"]} data-popover={placement}>
             <summary className={styles["popover-btn"]}>{label}</summary>
-            <div className={styles.popover}>{children}</div>
+            <div ref={popover} className={styles.popover}>{children}</div>
         </details>
     )
 }
